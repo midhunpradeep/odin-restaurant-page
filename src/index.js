ec2c61785ba1a265f812d90a4456e570f1cc9b06
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-12 00:19:57 +0100

    fix: Fix incorrect parents

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { createChildElement } from "./utility/utility";
 import homepage from "./homepage/homepage";
 
-const contentElement = createChildElement("div", document);
+const contentElement = createChildElement("div", document.body);
 contentElement.id = "content";
 
-const heading = createChildElement("h1", homepage);
+const heading = createChildElement("h1", contentElement);
 heading.textContent = "Culinary Delights";
 
-const subtitle = createChildElement("h2", homepage);
+const subtitle = createChildElement("h2", contentElement);
 subtitle.textContent = "Where exquisite flavors meet warm hospitality.";
 
 contentElement.appendChild(homepage);
---
+++ src/menu-page/menu-page.js
@@ -0,0 +1,5 @@
+export { menuPage as default };
+
+import { createChildElement } from "../utility/utility";
+
+const menuPage = document.createElement("div");
